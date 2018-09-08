6174ccec7d3a6c7c9ba07d0a12db0be648b93e17
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-08 22:56:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["Hello", "I", "am", "learning"]
+  array = ["Hello", "I", "am", "learning", "arrays"]
   array [-1]
 end
