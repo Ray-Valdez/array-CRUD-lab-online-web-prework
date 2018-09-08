5796d2b87d802e255960926dcb8eed644812ab4d
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-08 22:52:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "Hello", "I", "am", "learning"]
-  array.shift 
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
