16b8b7dd49ea36c5be22b0c5793b998637705aad
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-08 22:33:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  add_element_to_end_of_array << "bb"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  add_element_to_start_of_array.unshift("bb")
 end
 
 def remove_element_from_end_of_array(array)
-
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
-
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  retrieve_last_element_from_array[-1]
 end
