28a6b2601800ee809c8ac95a93d4e26e4a59917a
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-08 22:45:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Hello", "I", "am", "learning"]
-  element = ["Arrays!"]
-  array << element
+  array << "Arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
