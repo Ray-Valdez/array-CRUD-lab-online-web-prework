c465c82a5ef3b6fc9e66894d8cfd36c128d0dfd4
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-08 22:55:40 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["Hello", "I", "am", "learning"]
+  array = ["wow", "Hello", "I", "am", "learning"]
   array[0]
 end
 
